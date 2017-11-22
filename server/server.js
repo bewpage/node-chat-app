9bc5e90e07b161ce95a46c22ed128d59f0838416
bewpage <b.jedlinski@bewpage.com>
2017-11-22 16:59:39 +0100

    init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+   console.log(`Started on port ${port}`)
+});
+
+
+
+
+
+
