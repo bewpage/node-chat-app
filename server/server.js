60cb0d53000d7615e19b539072417ecea105a485
bewpage <b.jedlinski@bewpage.com>
2017-11-23 12:32:54 +0100

    server and socket.io config

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+// const server = http.Server(app);
+const io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+   console.log('New user connected');
+
+   socket.on('disconnect', () => {
+       console.log('User is disconnected');
+   })
+});
+
+server.listen(port, () => {
    console.log(`Started on port ${port}`)
 });
 
